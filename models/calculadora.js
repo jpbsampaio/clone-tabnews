5cc4191c0c384d71189ea595685efa9c6fd5ede6
João Paulo <jpbsampaio@hotmail.com>
2024-10-06 23:39:46 +0000

    add `/api/v1/status` endpoint

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(arg1, arg2) {
-  if (typeof arg1 !== 'number' || typeof arg2 !== 'number') {
-    throw new Error('Os argumentos precisam ser números');
-  }
-  return arg1 + arg2;
-}
-
-exports.somar = somar;
