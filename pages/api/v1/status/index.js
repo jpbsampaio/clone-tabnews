83d88dfb2990bd095b052a9a5bdc9bc6277e36fb
João Paulo <jpbsampaio@hotmail.com>
2025-01-15 21:11:17 +0000

    Adiciona arquivo `jsconfig.json`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from '../../../../infra/database.js';
+import database from 'infra/database.js';
 
 async function status(request, response) {
   const result = await database.query('SELECT 1 + 1 as sum;')
   console.log(result.rows);
-  response.status(200).json({ status: 'API está funcionando' });
+  response.status(200).json({ chave: 'API está funcionando' });
 }
 
 export default status;
