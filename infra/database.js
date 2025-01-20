13a4e92851bd4aa98ef99957fa5c4b7862ba019c
João Paulo <jpbsampaio@hotmail.com>
2025-01-20 22:21:35 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,8 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
-  console.log('credenciais do pg:', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  })
   
   try {
     await client.connect();
