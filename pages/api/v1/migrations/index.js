eb2d93a7357940fd48e667ddf2a4a829154321b8
João Paulo <jpbsampaio@hotmail.com>
2025-01-27 19:11:10 +0000

    Add `jest.config.js` and refactor migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,27 +2,31 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  if (request.method === "GET") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join("infra", "migrations"), // Feito por conta de SOs tratarem caminhos de formas diferentes
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    return response.status(200).json(migrations);
+  const defaultMigrationOption = {
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"), // Feito por conta de SOs tratarem caminhos de formas diferentes
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
   }
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOption);
+    return response.status(200).json(pendingMigrations);
+  }
+
   if (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOption,
       dryRun: false,
-      dir: join("infra", "migrations"), // Feito por conta de SOs tratarem caminhos de formas diferentes
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     })
-    return response.status(200).json(migrations);
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+    return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end();
