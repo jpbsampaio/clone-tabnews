3f805d6b91bbe83261c22d35a82c03f4b6f2965a
João Paulo <jpbsampaio@hotmail.com>
2025-02-02 20:40:37 +0000

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,11 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import { database } from "infra/database";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOption = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"), // Feito por conta de SOs tratarem caminhos de formas diferentes
     direction: "up",
@@ -13,15 +15,18 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOption);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
-
+  
   if (request.method === "POST") {
     const migratedMigrations = await migrationRunner({
       ...defaultMigrationOption,
       dryRun: false,
     })
+    await dbClient.end();
 
+    
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
     }
