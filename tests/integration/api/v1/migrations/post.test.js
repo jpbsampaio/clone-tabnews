38451521338a90fea93f89ef54b7204f5a77e371
João Paulo <jpbsampaio@hotmail.com>
2025-02-12 15:39:15 +0000

    fix `prettier` style lint

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -2,9 +2,9 @@ import database from "infra/database.js";
 import orchertrator from "tests/orchertrator.js";
 
 beforeAll(async () => {
-  await orchertrator.waitForAllServices()
+  await orchertrator.waitForAllServices();
   await database.query("drop schema public cascade; create schema public;");
-})
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
